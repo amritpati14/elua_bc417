b424f46c66fc0e1528f5b2e46896f30c627f0e45
Gabriel Duarte <gduarte.costa@globomail.com>
2013-08-04 21:02:29 -0300

    license added and prototype funcs added to be implemented

--- bc417.lua
+++ bc417.lua
@@ -1,6 +1,32 @@
+--[[
+The MIT License (MIT)
+
+Copyright (c) 2013 Gabriel Duarte (gduarte.costa@globomail.com)
+Copyright (c) 2013 Thuener Silva (thuener@gmail.com)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+--]]
+
+
+
 module("bc417", package.seeall)
 
-
+-- setup UART connection to bc415 module, must be called before any other funciontion related to the module
 function bc417_setup(uartid, baud)
   -- mbed has a special conf for pins
   if pb.board() == "MBED" then
@@ -20,5 +46,23 @@ function bc417_setup(uartid, baud)
   end -- end of mbed conf
     
     baud = uart.setup(uartid,  baud, 8, uart.PAR_NONE, uart.STOP_1)
+    return baud
+
     end
+end
+
+
+
+
+function bc417_set_pin()
+end
+
+
+function bc417_set_baud()
+end
+
+function bc417_set_name()
+end
+
+function bc417_listen()
 end
